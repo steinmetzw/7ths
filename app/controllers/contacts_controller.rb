a0c0becacb1d835b4218ecf72b3dddbd56ca0c31
William Steinmetz <cpacitanimulli@gmail.com>
2018-09-07 07:01:31 +0000

    Improved flash style

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
      flash[:success] = "Thank you for contacting us. Responses may take 1-2 days."
      redirect_to new_contact_path
    else
-     flash[:error] = @contact.errors.full_messages.join(", ")
+     flash[:danger] = @contact.errors.full_messages.join(", ")
      redirect_to new_contact_path
    end
  end
